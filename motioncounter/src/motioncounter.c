b83be3e19570f0f1f8df881d841bd62529119ffb
mahikgot <mahikgot@gmail.com>
2021-08-31 02:31:45 +0800

    fix number defines and put temporary cycle algo

--- motioncounter/src/motioncounter.c
+++ motioncounter/src/motioncounter.c
@@ -3,7 +3,7 @@
 #include "rcc.h"
 
 // connect to port A pins from A to F with A as MSB
-#define num0 0x3E
+#define num0 0x7E
 #define num1 0x30
 #define num2 0x6D
 #define num3 0x79
@@ -14,14 +14,68 @@
 #define num8 0x7F
 #define num9 0x73
 
-void light(uint32_t num)
+void delay(void)
 {
-	GPIOA->ODR = num;
+	uint32_t time = 0;
+	while (time < 500000)
+	{
+		time++;
+	}
+	time = 0;
+}
+void light(uint8_t num)
+{
+	switch(num)
+	{
+		case 0:
+			GPIOA->ODR = num0;
+			break;
+		case 1:
+			GPIOA->ODR = num1;
+			break;
+		case 2:
+			GPIOA->ODR = num2;
+			break;
+		case 3:
+			GPIOA->ODR = num3;
+			break;
+		case 4:
+			GPIOA->ODR = num4;
+			break;
+		case 5:
+			GPIOA->ODR = num5;
+			break;
+		case 6:
+			GPIOA->ODR = num6;
+			break;
+		case 7:
+			GPIOA->ODR = num7;
+			break;
+		case 8:
+			GPIOA->ODR = num8;
+			break;
+		case 9:
+			GPIOA->ODR = num9;
+			break;
+	}
 }
 int main(void)
 {
 	RCC_GPIOx_enable(GPIOA);
 	GPIOA->CRL = 0x22222222;
-	light(num7);
-	
+	uint8_t temp = 0;
+	while (1)
+	{
+		if (temp < 10)
+		{
+			light(temp);
+			delay();
+			temp++;
+		}
+		else
+		{
+			temp = 0;
+		}
+	}
 }
+
